932f224a90bffd89e43e9e724c2623472d6a8d9b
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2020-10-03 22:01:44 +0100

    Write environmental concentration data to InfluxDB

--- pisense.py
+++ pisense.py
@@ -1,13 +1,18 @@
 import time
 import serial
+from influxdb import InfluxDBClient
 
 from pm25 import plantower_pm25
 
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+# Create a client to write data to InfluxDB
+influx_client = InfluxDBClient(database='pisense')
+
+# create database if it does not already exist
+influx_client.create_database('pisense')
 
 # Connect to a PM2.5 sensor over UART
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 pm25_sensor = plantower_pm25.PM25_UART(uart)
-
 print("Found PM2.5 sensor, reading data...")
 
 while True:
@@ -41,3 +46,18 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
+
+    json_data = [
+        {
+            "measurement": "environmental_pm",
+            "tags" : {
+                "location": "indoors"
+            },
+            "fields": {
+                "pm10": aqdata["pm10 env"],
+                "pm25": aqdata["pm25 env"],
+                "pm100": aqdata["pm100 env"]
+            }
+        }
+    ]
+    influx_client.write_points(json_data)
