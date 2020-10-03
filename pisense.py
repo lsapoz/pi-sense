867d9c9e21cc5f13f2aa15a8467f5fe49496ab7f
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2020-10-03 20:45:51 +0100

    Set up script to read from plantower pm25 sensor Pull in adafruit sensor code and simplify dependencies Add requirements file and declare pyserial as a dependency

--- pisense.py
+++ pisense.py
@@ -1,1 +1,43 @@
-print("Hello World!")
+import time
+import serial
+
+from pm25 import plantower_pm25
+
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+
+# Connect to a PM2.5 sensor over UART
+pm25_sensor = plantower_pm25.PM25_UART(uart)
+
+print("Found PM2.5 sensor, reading data...")
+
+while True:
+    time.sleep(1)
+
+    try:
+        aqdata = pm25_sensor.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+
+    print()
+    print("Concentration Units (standard)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
+    print("Concentration Units (environmental)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
+    print("---------------------------------------")
+    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    print("---------------------------------------")
