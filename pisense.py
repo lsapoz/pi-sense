1c34c0b202da042cd170eca63a15a0f05be6b129
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2020-10-06 03:19:40 +0100

    Log temp, himidity, and pressure readings from BME280 Refactor to use daemon threads for polling

--- pisense.py
+++ pisense.py
@@ -1,8 +1,97 @@
 import time
+import threading
+
 import serial
+import board
+import busio
+import adafruit_bme280
+
 from influxdb import InfluxDBClient
 
 from pm25 import plantower_pm25
+
+def monitor_pm25(pm25_sensor: plantower_pm25.PM25, influx_client: InfluxDBClient):
+    # store the previous data point to compare with next reading
+    # the sensor outputs data faster than its own sample rate
+    # to avoid the duplicate readings, we'll avoid logging data that hasn't changed
+    # it's pretty unlikely that no readings change when the sensor actually re-samples the air
+    previous_data = {}
+    
+    while True:
+        # throttle
+        time.sleep(.5)
+
+        try:
+            aqdata = pm25_sensor.read()
+        except RuntimeError as err:
+            print(f"Unable to read from PM2.5 sensor ({err}), retrying...")
+            continue
+
+        if aqdata == previous_data:
+            print("PM2.5 data did not change, skipping...")
+            continue
+
+        previous_data = aqdata.copy()
+
+        print()
+        print("Concentration Units (standard)")
+        print("---------------------------------------")
+        print(
+            "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+            % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+        )
+        print("Concentration Units (environmental)")
+        print("---------------------------------------")
+        print(
+            "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+            % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+        )
+        print("---------------------------------------")
+        print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+        print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+        print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+        print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+        print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+        print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+        print("---------------------------------------")
+
+        json_data = [
+            {
+                "measurement": "environmental_pm",
+                "fields": {
+                    "pm10": aqdata["pm10 env"],
+                    "pm25": aqdata["pm25 env"],
+                    "pm100": aqdata["pm100 env"]
+                }
+            }
+        ]
+        influx_client.write_points(json_data)
+
+
+def monitor_bme280(bme_280: adafruit_bme280.Adafruit_BME280, influx_client: InfluxDBClient):
+    readings = {}
+
+    while True:
+        # throttle
+        time.sleep(10)
+
+        readings['temperature'] = bme280.temperature
+        readings['humdity'] = bme280.humidity
+        readings['pressure'] = bme280.pressure
+
+        print(f"BME280 - Temp:{readings['temperature']}°C Hum:{readings['humdity']}% P:{readings['pressure']}hPa")
+
+        json_data = []
+        for measurement, value in readings.items():
+            json_data.append(
+                {
+                    "measurement": measurement,
+                    "fields": {
+                        "value": value
+                    }
+                }   
+            )
+        influx_client.write_points(json_data)
 
 # Create a client to write data to InfluxDB
 influx_client = InfluxDBClient(database='pisense')
@@ -13,59 +102,20 @@ influx_client.create_database('pisense')
 # Connect to a PM2.5 sensor over UART
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 pm25_sensor = plantower_pm25.PM25_UART(uart)
-print("Found PM2.5 sensor, reading data...")
+print("Found PM2.5 sensor")
 
-# store the previous data point to compare with next reading
-# the sensor outputs data faster than its own sample rate
-# to avoid the duplicate readings, we'll avoid logging data that hasn't changed
-# it's pretty unlikely that no readings change when the sensor actually re-samples the air
-previous_data = {}
+# Connect to BME280 over I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+print("Found BME280")
 
-while True:
-    time.sleep(.5)
+# set up a daemon thread to poll each sensor
+polls = []
+polls.append(threading.Thread(target=monitor_pm25, args=(pm25_sensor, influx_client), daemon=True))
+polls.append(threading.Thread(target=monitor_bme280, args=(bme280, influx_client), daemon=True))
 
-    try:
-        aqdata = pm25_sensor.read()
-    except RuntimeError as err:
-        print(f"Unable to read from sensor ({err}), retrying...")
-        continue
-
-    if aqdata == previous_data:
-        print("Data did not change, skipping...")
-        continue
-
-    previous_data = aqdata.copy()
-
-    print()
-    print("Concentration Units (standard)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    )
-    print("Concentration Units (environmental)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
-
-    json_data = [
-        {
-            "measurement": "environmental_pm",
-            "fields": {
-                "pm10": aqdata["pm10 env"],
-                "pm25": aqdata["pm25 env"],
-                "pm100": aqdata["pm100 env"]
-            }
-        }
-    ]
-    influx_client.write_points(json_data)
+# start each polling thread and call join to run forever
+for t in polls:
+    t.start()
+for t in polls:
+    t.join()
